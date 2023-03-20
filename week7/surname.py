1ce9b353011880fdccaaea8007f08b07bb895d3a
Konstantin Kocev <konstantinkocev03@gmail.com>
2023-03-20 18:29:34 +0200

    Commit exercises week7

--- week7/surname.py
+++ week7/surname.py
@@ -1,11 +1,3 @@
-"""Във файл surname.py напишете функция taken_name(surname_husband, surname_wife), която:
-приема два аргумента - фамилията на мъжа и фамилията на жената.
-функцията връща True, ако жената е взела фамилията на мъжа и False, ако не е.
-Примери:
-
-"""
-
-
 def taken_name(surname_husband, surname_wife):
     if surname_husband in surname_wife:
         return True
